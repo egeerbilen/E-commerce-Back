7c27f0f3e23258ef7bbeea2cc96a0050a98f7ff0
DESKTOP-9AE13CG\Ege Erbilen <gegeerb2635@gmail.com>
2024-07-07 15:49:06 +0300

    .

--- Repository/Configurations/UserBasketConfiguration.cs
+++ Repository/Configurations/UserBasketConfiguration.cs
@@ -2,31 +2,34 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 
-public class UserBasketConfiguration : IEntityTypeConfiguration<UserBaskets>
+namespace Repository.Configurations
 {
-    public void Configure(EntityTypeBuilder<UserBaskets> builder)
+    internal class UserBasketConfiguration : IEntityTypeConfiguration<UserBaskets>
     {
-        // Tablo adı
-        builder.ToTable("UserBaskets");
+        internal void Configure(EntityTypeBuilder<UserBaskets> builder)
+        {
+            // Tablo adı
+            builder.ToTable("UserBaskets");
 
-        // Birleşik anahtar
-        builder.HasKey(b => new { b.UserId, b.ProductId });
+            // Birleşik anahtar
+            builder.HasKey(b => new { b.UserId, b.ProductId });
 
-        // Özellikler
-        builder.Property(b => b.UserId).IsRequired();
-        builder.Property(b => b.ProductId).IsRequired();
+            // Özellikler
+            builder.Property(b => b.UserId).IsRequired();
+            builder.Property(b => b.ProductId).IsRequired();
 
-        builder.HasOne(ub => ub.User)
-               .WithMany(u => u.UserBaskets)
-               .HasForeignKey(ub => ub.UserId)
-               .OnDelete(DeleteBehavior.Restrict); 
+            builder.HasOne(ub => ub.User)
+                   .WithMany(u => u.UserBaskets)
+                   .HasForeignKey(ub => ub.UserId)
+                   .OnDelete(DeleteBehavior.Restrict);
 
-        builder.HasOne(ub => ub.Product)
-               .WithMany(p => p.UserBaskets)
-               .HasForeignKey(ub => ub.ProductId)
-               .OnDelete(DeleteBehavior.Restrict); 
+            builder.HasOne(ub => ub.Product)
+                   .WithMany(p => p.UserBaskets)
+                   .HasForeignKey(ub => ub.ProductId)
+                   .OnDelete(DeleteBehavior.Restrict);
 
-        // İndeksler
-        builder.HasIndex(b => b.UserId).IsUnique(false);
+            // İndeksler
+            builder.HasIndex(b => b.UserId).IsUnique(false);
+        }
     }
 }
